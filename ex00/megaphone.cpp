ff9e3cccd411bdc0348bc68f7f3f7733d055bc6e
lopjf <lorispjf@gmail.com>
2023-08-25 11:28:11 +0100

    ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+
+int main(int argc, char *argv[]) {
+    if (argc == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else {
+        for (int i = 1; i < argc; i++) {
+            for (size_t j = 0; j < strlen(argv[i]); j++) {
+                if (argv[i][j] >= 97 && argv[i][j] <= 122)
+                    argv[i][j] -= 32;
+            }
+            std::cout << argv[i];
+        }
+        std::cout << std::endl;
+    }
+
+    return (0);
+}   

e1f9a10cdad3b8df05c0764f4507de487303acd0
lopjf <lorispjf@gmail.com>
2023-08-30 12:35:06 +0100

    module01

---
+++ module01/ex01/Zombie.cpp
@@ -0,0 +1,9 @@
+#include "Zombie.hpp"
+
+void Zombie( void ) {
+    return;
+}
+
+void ~Zombie( void ) {
+    return;
+}
---
+++ module01/ex01/Zombie.hpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#ifndef ZOMBIE_H
+# define ZOMBIE_H
+
+Zombie* zombieHorde( int N, std::string name );
+
+#endif

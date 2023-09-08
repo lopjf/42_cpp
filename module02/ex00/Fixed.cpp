7cde56d636adf49632cfe71dd4f5dee925edf807
Loris pierre jean-francois Penelon <lpenelon@c2r1s4.42lisboa.com>
2023-09-08 16:59:31 +0100

    ex00 done

--- module02/ex00/Fixed.cpp
+++ module02/ex00/Fixed.cpp
@@ -9,7 +9,7 @@ Fixed::Fixed() : _fpValue(0)
 Fixed::Fixed(const Fixed &copy)
 {
 	std::cout << "Copy Constructor called" << std::endl;
-	(void) copy;
+	*this = copy;
 }
 
 
@@ -23,16 +23,17 @@ Fixed::~Fixed()
 // Operators
 Fixed & Fixed::operator=(const Fixed &assign)
 {
-	(void) assign;
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->setRawBits(assign.getRawBits());
 	return *this;
 }
 
 int Fixed::getRawBits( void ) const {
 	std::cout << "getRawBits member function called" << std::endl;
-	return 0;
+	return this->_fpValue;
 }
 
 void Fixed::setRawBits( int const raw ) {
-	(void)raw;
+	this->_fpValue = raw;
 	return;
 }

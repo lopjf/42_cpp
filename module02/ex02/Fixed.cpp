b8ae98fb0240d5e8a18e63eb20d37cf390315aaf
lopjf <lorispjf@gmail.com>
2023-09-11 11:03:23 +0100

    ex02

---
+++ module02/ex02/Fixed.cpp
@@ -0,0 +1,158 @@
+#include "Fixed.hpp"
+
+// Constructors
+Fixed::Fixed(void) : _fpValue(0)
+{
+	std::cout << "Default Constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &copy)
+{
+	std::cout << "Copy Constructor called" << std::endl;
+	*this = copy;
+}
+
+Fixed::Fixed(const int integer) {
+	std::cout << "Int constructor called" << std::endl;
+	this->setRawBits(integer * (1 << this->_nbOfBits));
+	// std::cout << this->getRawBits() << std::endl;
+}
+
+Fixed::Fixed(const float number) {
+	std::cout << "Float constructor called" << std::endl;
+	this->setRawBits(static_cast<int>(roundf(number * static_cast<float>(1 << this->_nbOfBits))));
+	// std::cout << this->getRawBits() << std::endl;
+}
+
+
+// Destructor
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+
+// Operators
+Fixed & Fixed::operator=(const Fixed &assign)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->setRawBits(assign.getRawBits());
+	return *this;
+}
+
+
+// Comparison operators
+Fixed & Fixed::operator>(const Fixed)
+{
+	return *this;
+}
+
+Fixed & Fixed::operator<(const Fixed)
+{
+	return *this;
+}
+
+Fixed & Fixed::operator>=(const Fixed)
+{
+	return *this;
+}
+
+Fixed & Fixed::operator<=(const Fixed)
+{
+	return *this;
+}
+
+Fixed & Fixed::operator==(const Fixed)
+{
+	return *this;
+}
+
+Fixed & Fixed::operator!=(const Fixed)
+{
+	return *this;
+}
+
+		
+// Arithmetic operators
+Fixed & Fixed::operator+(const Fixed)
+{
+	return *this;
+}
+
+Fixed & Fixed::operator-(const Fixed)
+{
+	return *this;
+}
+
+Fixed & Fixed::operator*(const Fixed)
+{
+	
+	return *this;
+}
+
+Fixed & Fixed::operator/(const Fixed)
+{
+	return *this;
+}
+
+
+// increment/decrement operators
+Fixed Fixed::operator++()
+{
+	return *this;
+}
+
+Fixed & Fixed::operator++(int)
+{
+	return *this;
+}
+
+Fixed Fixed::operator--()
+{
+	return *this;
+}
+
+Fixed & Fixed::operator--(int)
+{
+	return *this;
+}
+
+
+int Fixed::getRawBits( void ) const {
+	// std::cout << "getRawBits member function called" << std::endl;
+	return this->_fpValue;
+}
+
+void Fixed::setRawBits( int const raw ) {
+	this->_fpValue = raw;
+	return;
+}
+
+float Fixed::toFloat( void ) const {
+	// that converts the fixed-point value to a floating-point value.
+
+	float ret;
+
+	ret = static_cast<float>(this->getRawBits()) / static_cast<float>(1 << this->_nbOfBits);
+	
+	return (ret);
+
+}
+
+int Fixed::toInt( void ) const {
+	// that converts the fixed-point value to an integer value.
+
+	int ret;
+
+	ret = this->getRawBits() / (1 << this->_nbOfBits);
+	
+	return (ret);
+
+}
+
+// Stream operators
+std::ostream & operator<<(std::ostream &stream, const Fixed &object)
+{
+	stream << object.toFloat();
+	return stream;
+}
---
+++ module02/ex02/Fixed.hpp
@@ -0,0 +1,59 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <string>
+# include <cmath>
+
+class Fixed
+{
+	public:
+		// Constructors
+		Fixed();
+		Fixed(const Fixed &copy);
+		Fixed(const int integer);
+		Fixed(const float number);
+		
+		// Destructor
+		~Fixed();
+		
+		// Copy operator
+		Fixed & operator=(const Fixed &assign);
+
+		// Comparison operators
+		Fixed & operator>(const Fixed);
+		Fixed & operator<(const Fixed);
+		Fixed & operator>=(const Fixed);
+		Fixed & operator<=(const Fixed);
+		Fixed & operator==(const Fixed);
+		Fixed & operator!=(const Fixed);
+		
+		// Arithmetic operators
+		Fixed & operator+(const Fixed);
+		Fixed & operator-(const Fixed);
+		Fixed & operator*(const Fixed);
+		Fixed & operator/(const Fixed);
+
+		// increment/decrement operators
+		Fixed operator++();
+		Fixed & operator++(int);
+		Fixed operator--();
+		Fixed & operator--(int);
+
+
+		int getRawBits( void ) const;
+		void setRawBits( int const raw );
+		float toFloat( void ) const;
+		int toInt( void ) const;
+
+	private:
+		int					_fpValue;
+		static const int	_nbOfBits = 8;
+};
+
+// Stream operators
+std::ostream & operator<<(std::ostream &stream, const Fixed &object);
+
+// An overload of the insertion («) operator that inserts a floating-point representation
+// of the fixed-point number into the output stream object passed as parameter.
+#endif

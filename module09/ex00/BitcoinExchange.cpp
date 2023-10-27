cdd2927297f0a001c017cbab5827c635bc952148
lopjf <lorispjf@gmail.com>
2023-10-27 12:23:41 +0100

    managed date check

--- module09/ex00/BitcoinExchange.cpp
+++ module09/ex00/BitcoinExchange.cpp
@@ -24,6 +24,36 @@ BitcoinExchange & BitcoinExchange::operator=(const BitcoinExchange &assign)
 	return *this;
 }
 
+// Exceptions
+const char * BitcoinExchange::WrongDate::what() const throw()
+{
+	return "Error: bad input.";
+}
+const char * BitcoinExchange::NegativeNumber::what() const throw()
+{
+	return "Error: not a positive number.";
+}
+const char * BitcoinExchange::OutOfBondNumber::what() const throw()
+{
+	return "Error: number is out of bond.";
+}
+
+void BitcoinExchange::getPrice(std::string str) // add csv file
+{
+	// std::cout << str << std::endl;
+	char *pEnd;
+	errno = 0;
+	long int year = strtol(str.c_str(), &pEnd, 10);
+	// strtol sets errno to != 0 if out of bond. Since it's a long int, the out of bond is higher than for int. pEnd will be set to what remains after the conversion. if conversion was impossible. pEnd will be the inputed stirng.
+	if (errno != 0 || year < INT_MIN || year > INT_MAX || pEnd[0] != '-')
+		throw WrongDate();
+	std::cout << year << std::endl;
+
+	// to continue in the bus
+	// should locate the lower closest date
+	// should then set a variable to the value get in data.csv
+	// (maybe compare the dates to get the right line?)
+}
 
 // Methods
 void BitcoinExchange::getValue(char *fileName)
@@ -31,10 +61,17 @@ void BitcoinExchange::getValue(char *fileName)
 	std::ifstream	ifs(fileName);
 	std::string		fileLine;
 
+	// open csv and store it in a variable
+
+
 	// get the file content if the file exist
 	if (ifs.is_open()) {
 		while (std::getline(ifs, fileLine)) {
-			std::cout << fileLine << std::endl;
+			try {
+				getPrice(fileLine);
+			} catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
+			}
 		}
 		ifs.close();
 	}
--- module09/ex00/BitcoinExchange.hpp
+++ module09/ex00/BitcoinExchange.hpp
@@ -3,6 +3,9 @@
 
 # include <iostream>
 #include <fstream>
+#include <stdlib.h>
+#include <string>
+#include <limits.h>
 
 class BitcoinExchange
 {
@@ -17,10 +20,26 @@ class BitcoinExchange
 		// Operators
 		BitcoinExchange & operator=(const BitcoinExchange &assign);
 		
+		// Exceptions
+		class WrongDate : public std::exception {
+			public:
+				virtual const char *what() const throw();
+		};
+		class NegativeNumber : public std::exception {
+			public:
+				virtual const char *what() const throw();
+		};
+		class OutOfBondNumber : public std::exception {
+			public:
+				virtual const char *what() const throw();
+		};
+
 		// Methods
 		// has to be static so we can call it from main without creating an instance of the class
 		static void getValue(char *fileName);
 	private:
+
+		static void getPrice(std::string str);
 		
 };
 
